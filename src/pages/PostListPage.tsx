e793a2d4d4f8e5682885889efa676d602a6b8c6f
_uchanlee <uchanlee.dev@gmail.com>
2020-08-19 08:41:12 +0900

    💪📗[UPDATE] <./src/pages/PostList.tsx> clean

--- src/pages/PostListPage.tsx
+++ src/pages/PostListPage.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import PostList from '../components/posts/PostList';
 
-const PostListPage: FC<{}> = () => <PostList />;
+const PostListPage: FC = () => <PostList />;
 
 export default PostListPage;
