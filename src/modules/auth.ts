e1b6775081c22dce9d13ed5d4a938aad58f3a7a3
_uchanlee <uchanlee.dev@gmail.com>
2020-08-18 16:52:46 +0900

    💪📗[UPDATE] <./src/modules/auth.ts> checkUser function

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,18 +1,37 @@
-import { atom } from 'recoil';
+import { atom, SetterOrUpdater } from 'recoil';
+import { UserState, check } from '../lib/api/auth';
 
-export const userState = atom<{
-  username: string;
-  workoutDays: number;
-  profileImage: string;
-  email: string;
-  loginType: string;
-}>({
+const userInitialState: UserState = {
+  username: '',
+  workoutDays: 0,
+  profileImage: '',
+  email: '',
+  loginType: '',
+};
+
+export const userState = atom<UserState>({
   key: 'auth/userState',
-  default: {
-    username: '',
-    workoutDays: 0,
-    profileImage: '',
-    email: '',
-    loginType: '',
-  },
+  default: userInitialState,
 });
+
+export const checkUser = (setUser: SetterOrUpdater<UserState>) => {
+  check()
+    .then(res => {
+      const { email, loginType, profileImage, username, workoutDays } = res.data;
+      setUser({
+        email,
+        loginType,
+        profileImage,
+        username,
+        workoutDays,
+      });
+    })
+    .catch(err => {
+      try {
+        localStorage.removeItem('user');
+        setUser(userInitialState);
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
+    });
+};
