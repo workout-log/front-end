b8b65ab6a07ee5535559ca7a1892345ca9aa299d
_uchanlee <uchanlee.dev@gmail.com>
2020-08-18 22:54:07 +0900

    💪📗[UPDATE] <./src/App.tsx> Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-import React, { FC, useEffect } from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { PostListPage, PostPage, WritePage, NoMatch } from './pages';
+import React, { FC } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { PostListPage, WritePage, PostPage, MyPagePage, NoMatch } from './pages';
 import { Header } from './components/common';
-import { writeState } from './modules/write';
-import MyPage from './components/mypage/MyPage';
 
-const App: FC<{}> = () => {
-  const location = useLocation();
-  const [write, setWrite] = useRecoilState(writeState);
-  useEffect(() => {
-    if (location.pathname !== '/write') {
-      setWrite({
-        title: '',
-        body: '',
-        tags: [],
-        files: new FormData(),
-        isPrivate: false,
-        isEditMode: false,
-      });
-    }
-  }, [location]);
+const App: FC = () => {
   return (
     <>
       <Header />
       <Switch>
         <Route component={PostListPage} path={['/@:username', '/']} exact />
-        <Route component={WritePage} path='/write' />
-        <Route component={PostPage} path='/@:username/:postId' />
-        <Route component={MyPage} path='/mypage' />
-        <Route component={NoMatch} />
+        <Route component={WritePage} path='/write' exact />
+        <Route component={PostPage} path='/@:username/:postId' exact />
+        <Route component={MyPagePage} path='/mypage' exact />
+        <Route component={NoMatch} path='/error' />
+        <Redirect to='/error' />
       </Switch>
-      <ins
-        className='kakao_ad_area'
-        style={{ display: 'none' }}
-        data-ad-unit='DAN-1iauxeaz75yr6'
-        data-ad-width='320'
-        data-ad-height='100'
-      ></ins>
     </>
   );
 };
