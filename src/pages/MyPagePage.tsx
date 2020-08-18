7f402ad63ecc834968b8c1399a2fb91de85ef7cb
_uchanlee <uchanlee.dev@gmail.com>
2020-08-19 08:40:51 +0900

    💪📗[UPDATE] <./src/pages/MyPagePage.tsx> clean

--- src/pages/MyPagePage.tsx
+++ src/pages/MyPagePage.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import MyPage from '../components/mypage/MyPage';
 
-const MyPagePage: FC<{}> = () => {
-  return <MyPagePage />;
-};
+const MyPagePage: FC = () => <MyPage />;
+
+export default MyPagePage;
