7f61eb8db2b01c99ede899057e471d3686936619
_uchanlee <uchanlee.dev@gmail.com>
2020-08-18 16:54:59 +0900

    💪📗[UPDATE] <./src/lib/api/auth.ts> UserState type

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,13 +1,22 @@
 import client from './client';
 
-export const login = ({ username, email, profileImage }) => client.post('/api/auth/login', { username, email, profileImage });
+export const login = (data: { username: string; email: string; profileImage: string }) => client.post('/api/auth/login', data);
 
-export const check = () => client.get('/api/auth/check');
+export type UserState = {
+  username: string;
+  workoutDays: number;
+  profileImage: string;
+  email: string;
+  loginType: string;
+};
+
+export const check = () => client.get<UserState>('/api/auth/check');
 
 export const logout = () => client.post('/api/auth/logout');
 
-export const update = (formData: FormData) => client.patch('/api/auth/user', formData, {
-  headers: { 'Content-Type': 'multipart/form-data' },
-});
+export const update = (formData: FormData) =>
+  client.patch('/api/auth/user', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
 
 export const leave = () => client.delete('/api/auth/user');
