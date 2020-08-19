5439dd5d80b37aec66058d67feee40df9c559d41
_uchanlee <uchanlee.dev@gmail.com>
2020-08-19 13:58:37 +0900

    💪📗[ADD] <./src/lib/function/index.ts> isLogin function

--- src/lib/function/index.ts
+++ src/lib/function/index.ts
@@ -1,7 +1,16 @@
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../modules/auth';
+import { Redirect } from 'react-router-dom';
+
 export const headerToggleHandler = () => {
   ($('#navbarCollapse') as any).collapse('toggle');
 };
 
 export const isGoogleImage = (image: string) => {
   return image.indexOf(':') !== -1 ? true : false;
+};
+
+export const isLogin = () => {
+  const { email } = useRecoilValue(userState);
+  return email !== '' ? true : false;
 };
