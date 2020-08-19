fbd6e9f9fcea67681b4a7e51f1a628bf1052affd
_uchanlee <uchanlee.dev@gmail.com>
2020-08-19 10:22:35 +0900

    💪📗[UPDATE] <./src/lib/api/posts.ts> clean

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,24 +1,37 @@
 import qs from 'querystring';
 import client from './client';
+import { UserState } from './auth';
+import { Comment } from './comments';
 
-export const writePost = (formData: FormData) => client.post('/api/posts', formData, {
-  headers: { 'Content-Type': 'multipart/form-data' },
-});
-
-export const readPost = (id) => client.get(`/api/posts/${id}`);
-
-export const listPosts = ({
-  page, username, useremail, tag,
-}) => {
-  const querystring = qs.stringify({
-    page, username, useremail, tag,
+export const writePost = (formData: FormData) =>
+  client.post('/api/posts', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
   });
-  return client.get(`/api/posts?${querystring}`);
+
+export type Post = {
+  id: number;
+  isPrivate: boolean;
+  title: string;
+  body: string;
+  files: string[];
+  tags: string[];
+  likeUsers: UserState[];
+  likes: number;
+  comments: Comment[];
+  user: UserState;
+  publishedDate: Date;
 };
 
-export const updatePost = (formData: FormData, id: number) => client.patch(`/api/posts/${id}`, formData,
-  { headers: { 'Content-Type': 'multipart/form-data' } });
+export const readPost = (id: number) => client.get<Post>(`/api/posts/${id}`);
 
-export const removePost = (id) => client.delete(`/api/posts/${id}`);
-const foo = 1;
-export default foo;
+type ListPostsData = { page: number; username: string; email: string; tag: string };
+
+export const listPosts = (data: ListPostsData) => {
+  const querystring = qs.stringify(data);
+  return client.get<Post[]>(`/api/posts?${querystring}`);
+};
+
+export const updatePost = (id: number, formData: FormData) =>
+  client.patch(`/api/posts/${id}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
+
+export const removePost = (id: number) => client.delete(`/api/posts/${id}`);
