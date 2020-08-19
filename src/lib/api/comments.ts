cdab27d7dd34da4739e83e8b92cedfa359c14420
_uchanlee <uchanlee.dev@gmail.com>
2020-08-19 10:00:51 +0900

    💪📗[UPDATE] <./src/lib/api/comments.ts> clean

--- src/lib/api/comments.ts
+++ src/lib/api/comments.ts
@@ -1,19 +1,24 @@
 import client from './client';
 
-export const writeComment = (id, text) => client.post(`/api/posts/${id}/comments`, {
-  text,
-});
+export const writeComment = (id: number, text: string) =>
+  client.post(`/api/posts/${id}/comments`, {
+    text,
+  });
 
-export const writeRecomment = (id, commentId, text) => client.post(`/api/posts/${id}/comments/${commentId}/recomments`, {
-  text,
-});
+export const writeRecomment = (id: number, commentId: number, text: string) =>
+  client.post(`/api/posts/${id}/comments/${commentId}/recomments`, {
+    text,
+  });
 
-export const listComments = (id) => client.get(`/api/posts/${id}/comments`);
+export const listComments = (id: number) => client.get(`/api/posts/${id}/comments`);
 
-export const updateComment = (id, commentId, text) => client.patch(`/api/posts/${id}/comments/${commentId}`, { text });
+export const updateComment = (id: number, commentId: number, text: string) =>
+  client.patch(`/api/posts/${id}/comments/${commentId}`, { text });
 
-export const updateRecomment = (id, commentId, recommentId, text) => client.patch(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`, { text });
+export const updateRecomment = (id: number, commentId: number, recommentId: number, text: string) =>
+  client.patch(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`, { text });
 
-export const deleteComment = (id, commentId) => client.delete(`/api/posts/${id}/comments/${commentId}`);
+export const deleteComment = (id: number, commentId: number) => client.delete(`/api/posts/${id}/comments/${commentId}`);
 
-export const deleteRecomment = (id, commentId, recommentId) => client.delete(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`);
+export const deleteRecomment = (id: number, commentId: number, recommentId: number) =>
+  client.delete(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`);
