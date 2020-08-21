9dfd6ce58508a398917308eb767058ed27d687cb
_uchanlee <uchanlee.dev@gmail.com>
2020-08-21 21:36:39 +0900

    💪📗[UPDATE] <./src/components/mypage/AskWithDrawModal.tsx> clean

--- src/components/mypage/AskWithDrawModal.tsx
+++ src/components/mypage/AskWithDrawModal.tsx
@@ -1,17 +1,19 @@
 import React, { FC } from 'react';
 import AskModal from '../common/AskModal';
 
-const AskWithDrawModal: FC<{
+type Props = {
   visible: boolean;
   onConfirm: () => void;
   onCancel: () => void;
-}> = ({ visible, onConfirm, onCancel }) => {
+};
+
+const AskWithDrawModal: FC<Props> = ({ visible, onConfirm, onCancel }) => {
   return (
     <AskModal
       visible={visible}
-      title="회원 탈퇴"
-      description="지금까지 올린 게시글이 삭제됩니다. 정말 탈퇴 하시겠습니까?"
-      confirmText="탈퇴"
+      title='회원 탈퇴'
+      description='지금까지 올린 게시글이 삭제됩니다. 정말 탈퇴 하시겠습니까?'
+      confirmText='탈퇴'
       onConfirm={onConfirm}
       onCancel={onCancel}
     />
