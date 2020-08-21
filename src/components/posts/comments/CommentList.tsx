89ee5616993e3fc91ac448a392e850c2c54bb004
_uchanlee <uchanlee.dev@gmail.com>
2020-08-21 21:41:14 +0900

    💪📗[UPDATE] <./src/components/posts/comments/CommentList.tsx> clean

--- src/components/posts/comments/CommentList.tsx
+++ src/components/posts/comments/CommentList.tsx
@@ -3,14 +3,16 @@ import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import CommentItem from './CommentItem';
+import RecommentList from './ReCommentList';
 import {
-  writeComment,
-  listComments,
-  writeRecomment,
+  commentsState,
+  Comment,
+  getComments,
   updateRecomment,
+  writeReComment,
   updateComment,
-} from '../../../lib/api/comments';
-import { commentsState, commentType } from '../../../modules/comments';
+  writeComment,
+} from '../../../modules/comments';
 import { userState } from '../../../modules/auth';
 
 const CommentListWrapper = styled.div`
@@ -84,173 +86,109 @@ const CommentListWrapper = styled.div`
   }
 `;
 
-const CommentList: FC<{}> = () => {
+const CommentList: FC = () => {
   const location = useLocation();
-  const postId = location.pathname.split('/')[2];
-  const [commentId, setCommentId] = useState(0);
-  const [recommentId, setRecommentId] = useState(0);
-  const [commentText, setCommentText] = useState('');
+  const postId = Number(location.pathname.split('/')[2]);
+
+  const [commentId, setCommentId] = useState<number>(0);
+  const [recommentId, setRecommentId] = useState<number>(0);
+
+  const [text, setText] = useState<string>('');
+
   const [commentsList, setCommentsList] = useRecoilState(commentsState);
-  const [isRecommentMode, setIsRecommentMode] = useState(false);
-  const user = useRecoilValue(userState);
-  const commentChangeHandler = useCallback((e) => {
-    setCommentText(e.target.value);
+
+  const [isRecomment, setIsRecomment] = useState<boolean>(false);
+
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+  const [isReplyMode, setIsReplyMode] = useState<boolean>(false);
+
+  const { email } = useRecoilValue(userState);
+  const onChange = useCallback(e => {
+    setText(e.target.value);
   }, []);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [isReplyMode, setIsReplyMode] = useState(false);
   const initMode = useCallback(() => {
-    setIsEditMode(false);
-    setIsReplyMode(false);
     setCommentId(0);
     setRecommentId(0);
-    setIsRecommentMode(false);
-    setCommentText('');
+    setIsRecomment(false);
+    setIsEditMode(false);
+    setIsReplyMode(false);
+    setText('');
   }, []);
   const commentSubmitHandler = useCallback(() => {
-    if (!user.email) return alert('로그인 후 이용해주세요.');
-    if (!commentText) return alert('1자 이상 입력해주세요.');
-
-    if (isRecommentMode) {
+    if (!email) return alert('로그인 후 이용해주세요.');
+    if (!text) return alert('1자 이상 입력해주세요.');
+    if (isRecomment) {
       if (isEditMode) {
-        updateRecomment(postId, commentId, recommentId, commentText)
-          .then((res) => {
-            setCommentsList(res.data);
-            initMode();
-          })
-          .catch((err) => console.log(err));
+        updateRecomment(postId, commentId, recommentId, text, setCommentsList, initMode);
       }
     } else {
       if (isReplyMode) {
-        writeRecomment(postId, commentId, commentText)
-          .then((res) => {
-            console.log(res);
-            console.log(
-              commentsList.map((c) => {
-                if (c.id === commentId)
-                  return {
-                    ...c,
-                    recomments: [
-                      ...c.recomments,
-                      {
-                        ...res.data,
-                      },
-                    ],
-                  };
-                else return c;
-              }),
-            );
-            setCommentsList(
-              commentsList.map((c) => {
-                if (c.id === commentId)
-                  return {
-                    ...c,
-                    recomments: [
-                      ...c.recomments,
-                      {
-                        ...res.data,
-                      },
-                    ],
-                  };
-                else return c;
-              }),
-            );
-            ($(`.tab-content > ul > div > .c-${res.data.id}`) as any).addClass(
-              'show',
-            );
-            initMode();
-          })
-          .catch((err) => console.log(err));
-        return;
+        writeReComment(postId, commentId, text, setCommentsList, initMode);
+      } else if (isEditMode) {
+        updateComment(postId, commentId, text, setCommentsList, initMode);
+      } else {
+        writeComment(postId, text, setCommentsList, initMode);
       }
-      if (isEditMode) {
-        updateComment(postId, commentId, commentText)
-          .then((res) => {
-            setCommentsList(res.data);
-            initMode();
-          })
-          .catch((err) => console.log(err));
-        return;
-      }
-      writeComment(postId, commentText)
-        .then((res) => {
-          setCommentsList([
-            ...commentsList,
-            {
-              ...res.data,
-            },
-          ]);
-          initMode();
-        })
-        .catch((err) => console.log(err.response));
     }
-  }, [
-    isRecommentMode,
-    isReplyMode,
-    isEditMode,
-    commentId,
-    recommentId,
-    commentText,
-  ]);
+  }, [isRecomment, isReplyMode, isEditMode, commentId, recommentId, text]);
+
   useEffect(() => {
-    listComments(postId)
-      .then((res) => {
-        setCommentsList(res.data);
-      })
-      .catch((err) => console.log(err));
+    getComments(postId, setCommentsList);
   }, []);
 
   return (
     <CommentListWrapper>
       <div>
-        <div className="page-header">
-          <h3 className="reviews">
+        <div className='page-header'>
+          <h3 className='reviews'>
             {isEditMode && '수정 하기'}
             {isReplyMode && '대댓글 달기'}
             {!isEditMode && !isReplyMode && '댓글 달기'}
           </h3>
           <div>
-            <textarea
-              value={commentText}
-              onChange={commentChangeHandler}
-            ></textarea>
+            <textarea value={text} onChange={onChange}></textarea>
             {(isEditMode || isReplyMode) && (
-              <button className="btn btn-danger" onClick={initMode}>
+              <button className='btn btn-danger' onClick={initMode}>
                 취소
               </button>
             )}
-            <button className="btn btn-success" onClick={commentSubmitHandler}>
+            <button className='btn btn-success' onClick={commentSubmitHandler}>
               {isEditMode ? '수정' : '작성'}
             </button>
           </div>
         </div>
-        <div className="comment-tab">
-          <ul className="nav nav-tabs" role="tablist">
-            <li className="active">
-              <h4 className="reviews text-capitalize">Comments</h4>
+        <div className='comment-tab'>
+          <ul className='nav nav-tabs' role='tablist'>
+            <li className='active'>
+              <h4 className='reviews text-capitalize'>Comments</h4>
             </li>
           </ul>
-          <div className="tab-content">
+          <div className='tab-content'>
             <ul>
-              {commentsList.map(
-                (
-                  d: commentType & {
-                    recomments: commentType[];
-                  },
-                ) => (
-                  <CommentItem
+              {commentsList.map((data: Comment) => (
+                <CommentItem
+                  data={data}
+                  commentId={data.id}
+                  setCommentId={setCommentId}
+                  setRecommentId={setRecommentId}
+                  isRecomment={false}
+                  setIsReplyMode={setIsReplyMode}
+                  setIsEditMode={setIsEditMode}
+                  key={`c_${data.id}`}
+                  setIsRecomment={setIsRecomment}
+                >
+                  <RecommentList
+                    data={data.recomments}
+                    commentId={data.id}
                     setCommentId={setCommentId}
-                    setRecommentId={setRecommentId}
-                    isReplyMode={isReplyMode}
-                    isEditMode={isEditMode}
                     setIsReplyMode={setIsReplyMode}
                     setIsEditMode={setIsEditMode}
-                    key={`c_${d.id}`}
-                    data={d}
-                    hasRecomment={d.recomments.length ? true : false}
-                    setIsRecommentMode={setIsRecommentMode}
+                    setRecommentId={setRecommentId}
+                    setIsRecomment={setIsRecomment}
                   />
-                ),
-              )}
+                </CommentItem>
+              ))}
             </ul>
           </div>
         </div>
