f620da27d53792f1e3f43c3b75fb19acbf86bd4c
_uchanlee <uchanlee.dev@gmail.com>
2020-08-21 21:33:23 +0900

    💪📗[UPDATE] <./lib/api/comments.ts> axios type

--- src/lib/api/comments.ts
+++ src/lib/api/comments.ts
@@ -1,5 +1,6 @@
 import client from './client';
 import { UserState } from './auth';
+import { Post } from './posts';
 
 export type CommonComment = {
   id: number;
@@ -20,7 +21,7 @@ export const writeComment = (id: number, text: string) =>
     text,
   });
 
-export const writeRecomment = (id: number, commentId: number, text: string) =>
+export const writeReComment = (id: number, commentId: number, text: string) =>
   client.post(`/api/posts/${id}/comments/${commentId}/recomments`, {
     text,
   });
@@ -30,10 +31,11 @@ export const listComments = (id: number) => client.get<Comment[]>(`/api/posts/${
 export const updateComment = (id: number, commentId: number, text: string) =>
   client.patch(`/api/posts/${id}/comments/${commentId}`, { text });
 
-export const updateRecomment = (id: number, commentId: number, recommentId: number, text: string) =>
+export const updateReComment = (id: number, commentId: number, recommentId: number, text: string) =>
   client.patch(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`, { text });
 
-export const deleteComment = (id: number, commentId: number) => client.delete(`/api/posts/${id}/comments/${commentId}`);
+export const deleteComment = (id: number, commentId: number) =>
+  client.delete<Post>(`/api/posts/${id}/comments/${commentId}`);
 
 export const deleteRecomment = (id: number, commentId: number, recommentId: number) =>
   client.delete(`/api/posts/${id}/comments/${commentId}/recomments/${recommentId}`);
