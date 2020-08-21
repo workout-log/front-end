6ab111fbc753202bb8b44b5c1f5e4c2cd3b39271
_uchanlee <uchanlee.dev@gmail.com>
2020-08-21 21:38:00 +0900

    💪📗[UPDATE] <./src/components/posts/PostViewer.tsx> clean

--- src/components/posts/PostViewer.tsx
+++ src/components/posts/PostViewer.tsx
@@ -1,16 +1,15 @@
-import React, { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { FC, useEffect, useState, useCallback } from 'react';
+import { useRouteMatch, useHistory, Redirect } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { postState, writeState } from '../../modules/write';
-import { readPost, removePost } from '../../lib/api/posts';
+import styled from 'styled-components';
 import { SubInfo, Tags } from '../common';
 import PostActionButtons from './PostActionButtons';
-import { userState } from '../../modules/auth';
 import { isLike, isNotLike, lockIcon } from '../../../public/assets';
-import { like, notLike } from '../../lib/api/liking';
-import { check } from '../../lib/api/auth';
 import CommentList from './comments/CommentList';
+import { getPost, removePost, bad, good } from '../../modules/posts';
+import { isLikeAndMine } from '../../lib/function';
+import { postState, editorState } from '../../modules/editor';
+import { userState } from '../../modules/auth';
 
 const PostViewerWrapper = styled.div`
   margin-top: 4rem;
@@ -19,8 +18,20 @@ const PostViewerWrapper = styled.div`
     max-height: 31rem;
     display: block;
     overflow-x: scroll;
+    overflow-y: hidden;
     margin-bottom: 1rem;
     white-space: nowrap;
+    ::-webkit-scrollbar {
+      width: 100%;
+      height: 8px;
+    }
+    ::-webkit-scrollbar-track {
+      background: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      background: #2c2c2c;
+      border-radius: 0;
+    }
     > embed {
       object-fit: cover;
       width: 100%;
@@ -71,146 +82,98 @@ const PostContent = styled.div`
   color: #343a40;
 `;
 
-const PostViewer: FC<
-  RouteComponentProps & {
-    match: {
-      params: {
-        postId: string;
-      };
-    };
-    history: {
-      push: (p: string) => {};
-    };
-  }
-> = ({ match, history }) => {
-  const { postId } = match.params;
+type Params = {
+  postId: string;
+};
+
+const PostViewer: FC = () => {
+  const [isLiked, isMine] = isLikeAndMine();
+  const match = useRouteMatch<Params>();
+  const history = useHistory();
+  const postId = Number(match.params.postId);
   const [error, setError] = useState('');
-  const user = useRecoilValue(userState);
-  const [post, setPost] = useRecoilState(postState);
-  const [write, setWrite] = useRecoilState(writeState);
+  const [
+    { title, body, tags, files, isPrivate, user, likes, publishedDate, id },
+    setPost,
+  ] = useRecoilState(postState);
+  const { email } = useRecoilValue(userState);
+  const [, setEditor] = useRecoilState(editorState);
   const [liking, setLiking] = useState(false);
-  useEffect(() => {
-    readPost(postId)
-      .then((res) => {
-        setPost(res.data);
-        check()
-          .then((userRes) => {
-            let user = {
-              email: userRes.data.email,
-              loginType: userRes.data.loginType,
-              profileImage: userRes.data.profileImage,
-              username: userRes.data.username,
-              workoutDays: userRes.data.workoutDays,
-            };
-            if (user) {
-              if (
-                res.data.likeUsers.findIndex((e) => e === user.email) === -1
-              ) {
-                setLiking(false);
-              } else {
-                setLiking(true);
-              }
-            } else setLiking(false);
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => {
-        if (err.response && err.response.status === 404) {
-          setError('not found');
-          return;
-        }
-        setError('error');
-      });
-  }, []);
+  if (isNaN(postId) || postId < 1) return <Redirect to='/error' />;
+
   const onEdit = () => {
-    setWrite({
-      title: post.title,
-      body: post.body,
-      tags: post.tags,
+    setEditor({
+      title,
+      body,
+      tags,
       files: new FormData(),
-      filesUrl: post.files,
-      isPrivate: post.isPrivate,
-      originalPostId: post.id,
+      filesUrl: files,
+      isPrivate,
+      originalPostId: id,
       isEditMode: true,
     });
     history.push('/write');
   };
   const onRemove = () => {
-    removePost(postId)
-      .then((res) => history.push('/'))
-      .catch((err) => console.log(err));
+    removePost(postId, history);
   };
-
-  const onLikeHandler = () => {
-    if (user.username === '') {
-      return alert('로그인 후 이용해주세요.');
-    }
+  const onLikeHandler = useCallback(() => {
+    if (!email) return alert('로그인 후 이용해주세요!');
     if (liking) {
-      notLike(postId)
-        .then((res) => {
-          setPost(res.data);
-          setLiking(false);
-        })
-        .catch((err) => console.log(err));
+      bad(postId, setPost, setLiking);
     } else {
-      like(postId)
-        .then((res) => {
-          setLiking(true);
-          setPost(res.data);
-        })
-        .catch((err) => console.log(err));
+      good(postId, setPost, setLiking);
     }
-  };
+  }, [liking]);
+
+  useEffect(() => {
+    getPost(postId, setPost, setError);
+  }, [postId]);
+  useEffect(() => {
+    setLiking(isLiked);
+  }, [isLiked]);
   if (error === 'not found')
     return (
       <PostViewerWrapper>
-        <div className="container">존재하지 않는 포스트입니다.</div>
+        <div className='container'>존재하지 않는 포스트입니다.</div>
       </PostViewerWrapper>
     );
   else if (error === 'error')
     return (
       <PostViewerWrapper>
-        <div className="container">오류 발생!</div>
+        <div className='container'>오류 발생!</div>
       </PostViewerWrapper>
     );
-  if (!Object.keys(post).length) return null;
-
   return (
     <PostViewerWrapper>
-      <div className="container">
+      <div className='container'>
         <PostHead>
           <h1>
-            {post.isPrivate && <img src={lockIcon} />}
-            {post.title}
+            {isPrivate && <img src={lockIcon} />}
+            {title}
           </h1>
           <SubInfo
-            username={post.user.username}
-            profileImage={post.user.profileImage}
-            publishedDate={post.publishedDate}
+            username={user.username}
+            profileImage={user.profileImage}
+            publishedDate={publishedDate}
             hasMarginTop
           />
-          <Tags tags={post.tags} />
+          <Tags tags={tags} />
         </PostHead>
-        <div className="additional">
-          <span>{post.likes}</span>
+        <div className='additional'>
+          <span>{likes}</span>
           <img src={liking ? isLike : isNotLike} onClick={onLikeHandler} />
-          {(user && user.email) === (post && post.user.email) && (
-            <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
-          )}
+          {isMine && <PostActionButtons onEdit={onEdit} onRemove={onRemove} />}
         </div>
-
-        <div id="files">
-          {post.files &&
-            post.files.length !== 0 &&
-            post.files.map((f) => (
-              <embed src={`${process.env.SERVER_URL}/${f}`} key={f} />
-            ))}
+        <div id='files'>
+          {!!!files.length &&
+            files.map(f => <embed src={`${process.env.SERVER_URL}/${f}`} key={f} />)}
         </div>
-        <PostContent dangerouslySetInnerHTML={{ __html: post.body }} />
+        <PostContent dangerouslySetInnerHTML={{ __html: body }} />
         <CommentList />
       </div>
     </PostViewerWrapper>
   );
 };
 
-export default withRouter(PostViewer);
+export default PostViewer;
